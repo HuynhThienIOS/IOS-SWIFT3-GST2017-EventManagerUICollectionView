f7a6249f5ddb340447d56bc968c2574b5078251b
Thiện Huỳnh <thienhuynh@Thiens-Mac.local>
2017-05-06 23:00:54 +0700

    fix some bug and add event

---
+++ EventManagerUICollectionView/EventAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  EventAdapter.swift
+//  EventManagerUICollectionView
+//
+//  Created by Thiện Huỳnh on 5/6/17.
+//  Copyright © 2017 Thiện Huỳnh. All rights reserved.
+//
+
+import Foundation
